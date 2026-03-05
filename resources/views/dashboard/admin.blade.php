c8508d814d8cd0f4e9acc5eea1ccbe8e7957c34d
Cristian <crisjefris@gmail.com>
2026-03-05 14:38:05 +0700

    test auto deploy

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title', 'Dashboard Admin')
-@section('page-title', 'Panel Kontrol Admin')
+@section('page-title', 'CICD BERHASIL')
 
 @section('content')
 <div class="stats-grid">
