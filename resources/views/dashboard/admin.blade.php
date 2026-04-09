552aaf4f7129692ea1fc6317b706241d24949429
Cristian <crisjefris@gmail.com>
2026-04-09 12:49:52 +0700

    edit button

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title', 'Dashboard Admin')
-@section('page-title', 'Dashboard Admin')
+@section('page-title', 'Tytyd Admin')
 
 @section('content')
 <div class="dashboard-shell">
